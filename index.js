c2a9176ee0cc603378494575180429e6d1903732
EdikCharkh <93819601+EdikCharkh@users.noreply.github.com>
2023-09-30 22:50:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let button_I = document.getElementById("button_I");
+
+let starter_II = document.getElementById("starter_II")
+
+
+
+
+
+function starter() {
+
+    button_I.style.visibility = "hidden"
+    
+    starter_II.style.display = "block"
+}   
